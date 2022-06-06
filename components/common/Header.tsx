131f9ef30199591ad9956926b42000ec319017fe
Martin Starosta <martin.starosta@labzone.tech>
2022-06-06 19:01:23 +0200

    Fix mobile nav

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -6,41 +6,37 @@ type HeaderProps = {};
 
 export default function Header({}: HeaderProps) {
   return (
-    <nav className="flex items-center justify-between container">
-      <Link href="/">
-        <a className="inline-flex items-center justify-center pt-8">
-          <Image
-            src="/images/dasato-logo.png"
-            alt="Logo Dasato Svet podla Gabiky"
-            width={80}
-            height={80}
-          />
-        </a>
-      </Link>
+    <div className="shadow-sm bg-white">
+      <nav className="block md:flex items-center justify-between container">
+        <Link href="/">
+          <a className="block mb-4 text-center md:inline-flex items-center justify-center pt-4">
+            <Image
+              src="/images/dasato-logo.png"
+              alt="Logo Dasato Svet podla Gabiky"
+              width={80}
+              height={80}
+            />
+          </a>
+        </Link>
 
-      <ul className="flex items-center space-x-2 text-sm font-medium text-gray-500">
-        <li>
+        <div className="menu-items flex justify-between items-center space-x-2 text-sm font-medium text-gray-500">
           <a className="px-3 py-2 rounded-lg" href="#o-projekte">
             O projekte
           </a>
-        </li>
 
-        <li>
           <a className="px-3 py-2 rounded-lg" href="#o-gabike">
             O Gabike
           </a>
-        </li>
-        <li>
+
           <a className="px-3 py-2 rounded-lg" href="#epizody">
             Epizódy
           </a>
-        </li>
-        <li>
+
           <a className="px-3 py-2 rounded-lg" href="#kontakt">
             Kontakt
           </a>
-        </li>
-      </ul>
-    </nav>
+        </div>
+      </nav>
+    </div>
   );
 }
